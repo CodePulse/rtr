2e3a8875839c5dda5f4b48606bea976624b6ea21
Ben Lamptey <ben@bigbluedoor.net>
2019-10-17 00:39:40 +0100

    #0:save status pages as well as job application form

--- web/modules/custom/rtr_utilities/src/Controller/StatusController.php
+++ web/modules/custom/rtr_utilities/src/Controller/StatusController.php
@@ -4,10 +4,16 @@ namespace Drupal\rtr_utilities\Controller;
 class StatusController {
 
   public function notFound() {
+    return [
+      '#theme' => 'rtr-404',
+    ];
 
   }
 
   public function accessDenied() {
+    return [
+      '#theme' => 'rtr-403',
+    ];
 
   }
 }
